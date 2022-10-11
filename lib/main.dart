11dcbc1b7fff9a62d3d8e0d33a43e7586a16ebea
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-11 16:26:41 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: JankenPage(),
+    );
+  }
+}
+
+class JankenPage extends StatefulWidget {
+  const JankenPage({super.key});
+
+  @override
+  State<JankenPage> createState() => _JankenPageState();
+}
+
+class _JankenPageState extends State<JankenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('じゃんけん'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('✊'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('✌️'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('✋'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
