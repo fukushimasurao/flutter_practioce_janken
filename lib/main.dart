d3c33f2013c136e40443cd3f99814e3693fa5aa8
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-11 16:54:18 +0900

    コンピューターの手をランダムで出す

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,12 +25,32 @@ class JankenPage extends StatefulWidget {
 }
 
 class _JankenPageState extends State<JankenPage> {
-  String myHand = '✊!!';
+  String myHand = '✊';
+  String computerHand = '✊';
 
   void selectHands(String selectedHand) {
     myHand = selectedHand;
-    print(selectedHand);
+
+    generatedComputerHand();
     setState(() {});
+  }
+
+  void generatedComputerHand() {
+    final randomNumber = Random().nextInt(3);
+    computerHand = randomNumberToHand(randomNumber);
+  }
+
+  String randomNumberToHand(int randomNumber) {
+    switch (randomNumber) {
+      case 0:
+        return '✊';
+      case 1:
+        return '✌️';
+      case 2:
+        return '✋';
+      default:
+        return '✊';
+    }
   }
 
   @override
@@ -41,6 +63,10 @@ class _JankenPageState extends State<JankenPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(computerHand, style: const TextStyle(fontSize: 32)),
+            const SizedBox(
+              height: 64,
+            ),
             Text(myHand, style: const TextStyle(fontSize: 32)),
             const SizedBox(height: 32),
             Row(
